ebcb33a6c2035ab76d3dd4a704127d2dfb777b69
Titanscull <ispetrovskyi@gmail.com>
2021-03-08 12:22:03 +0200

    Text Field Delegates set up, layers corners set up

--- WA_Exam_App/WA_Exam_App/Controller/RegistrationViewController.swift
+++ WA_Exam_App/WA_Exam_App/Controller/RegistrationViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
-
+    
     @IBOutlet weak var regErrorLabel: UILabel!
     @IBOutlet weak var userImageView: UIImageView!
     
@@ -24,13 +24,62 @@ class RegistrationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //regErrorLabel.isHidden = true
+        setFieldCOrners()
+        setRegTextFieldDelegates()
+        
+    }
+    
+    
+    
+    /// Makes rounded corners
+    func setFieldCOrners() {
+        self.firstNameTextField.layer.masksToBounds = true
+        self.lastNameTextField.layer.masksToBounds = true
+        self.usernameTextField.layer.masksToBounds = true
+        self.passwordTextField.layer.masksToBounds = true
+        self.checkPasswordTextField.layer.masksToBounds = true
+        
+        firstNameTextField.layer.cornerRadius = 15
+        lastNameTextField.layer.cornerRadius = 15
+        usernameTextField.layer.cornerRadius = 15
+        passwordTextField.layer.cornerRadius = 15
+        checkPasswordTextField.layer.cornerRadius = 15
+    }
+    
+    /// Setting delegates for text fields
+    func setRegTextFieldDelegates() {
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        checkPasswordTextField.delegate = self
     }
     
     /// Saves correct data to model
     @IBAction func saveDataButton(_ sender: UIButton) {
     }
     
+}
+
+/// Text fields operating delegates
+extension RegistrationViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        firstNameTextField.resignFirstResponder()
+        lastNameTextField.resignFirstResponder()
+        usernameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        checkPasswordTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        regErrorLabel.isHidden = true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
