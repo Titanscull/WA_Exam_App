838e90f658351f068a184cca8f94ded02efc784d
Titanscull <ispetrovskyi@gmail.com>
2021-03-08 11:12:14 +0200

    setFields() added - cornering radius of text fields

--- WA_Exam_App/WA_Exam_App/Controller/LoginViewController.swift
+++ WA_Exam_App/WA_Exam_App/Controller/LoginViewController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setFields()
+        
     }
     
+    func setFields() {
+        self.usernameTextField.layer.masksToBounds = true
+        self.passwordTextField.layer.masksToBounds = true
+        usernameTextField.layer.cornerRadius = 20
+        passwordTextField.layer.cornerRadius = 20
+    }
+    
+    /// Login Button
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    /// Registration Button
+    @IBAction func registrationButton(_ sender: Any) {
+    }
 }
+
+
+
+
 
