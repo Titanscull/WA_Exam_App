ee33af74084e43abced583fd6995d050206779e9
Titanscull <ispetrovskyi@gmail.com>
2021-03-10 20:38:47 +0200

    Registration checking revisited

--- WA_Exam_App/WA_Exam_App/Controller/RegistrationViewController.swift
+++ WA_Exam_App/WA_Exam_App/Controller/RegistrationViewController.swift
@@ -15,7 +15,7 @@ class RegistrationViewController: UIViewController {
     
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var checkPasswordTextField: UITextField!
@@ -35,13 +35,13 @@ class RegistrationViewController: UIViewController {
     func setFieldCorners() {
         self.firstNameTextField.layer.masksToBounds = true
         self.lastNameTextField.layer.masksToBounds = true
-        self.usernameTextField.layer.masksToBounds = true
+        self.userNameTextField.layer.masksToBounds = true
         self.passwordTextField.layer.masksToBounds = true
         self.checkPasswordTextField.layer.masksToBounds = true
         
         firstNameTextField.layer.cornerRadius = 15
         lastNameTextField.layer.cornerRadius = 15
-        usernameTextField.layer.cornerRadius = 15
+        userNameTextField.layer.cornerRadius = 15
         passwordTextField.layer.cornerRadius = 15
         checkPasswordTextField.layer.cornerRadius = 15
     }
@@ -50,7 +50,7 @@ class RegistrationViewController: UIViewController {
     func setRegTextFieldDelegates() {
         firstNameTextField.delegate = self
         lastNameTextField.delegate = self
-        usernameTextField.delegate = self
+        userNameTextField.delegate = self
         passwordTextField.delegate = self
         checkPasswordTextField.delegate = self
     }
@@ -58,75 +58,118 @@ class RegistrationViewController: UIViewController {
     /// Saves correct data to model
     @IBAction func saveDataButton(_ sender: UIButton) {
         
-        /// Firstname check
-        if firstNameTextField.text == "" {
-            firstNameTextField.backgroundColor = .red
+        let name = firstNameTextField.text!
+        let surName = lastNameTextField.text!
+        let userName = userNameTextField.text!
+        let password = passwordTextField.text!
+        let checkedPassword = checkPasswordTextField.text!
+        
+        /// Checking for input of Users data
+        if name.isEmpty || surName.isEmpty ||  password.isEmpty || checkedPassword.isEmpty {
+            if name.isEmpty {
+                firstNameTextField.backgroundColor = .red
+                print("Name field is empty")
+            }
+            if surName.isEmpty {
+                lastNameTextField.backgroundColor = .red
+                print("Surname field is empty")
+            }
+            if userName.isEmpty {
+                userNameTextField.backgroundColor = .red
+                print("Username field is empty")
+            }
+            if password.isEmpty {
+                passwordTextField.backgroundColor = .red
+                print("Password is empty")
+            }
+            if checkedPassword.isEmpty {
+                checkPasswordTextField.backgroundColor = .red
+                print("Check password is empty")
+            }
             regErrorLabel.isHidden = false
-            regErrorLabel.text = "First name field is empty"
-            print(regErrorLabel.text!)
+            regErrorLabel.text = "All fields required"
             return
         }
         
-        /// Last name check
-        if lastNameTextField.text == "" {
-            lastNameTextField.backgroundColor = .red
-            regErrorLabel.isHidden = false
-            regErrorLabel.text = "Last name field is empty"
-            print(regErrorLabel.text!)
-            return
-        }
-        
-        /// UserName check
-        if usernameTextField.text == "" {
-            usernameTextField.backgroundColor = .red
-            regErrorLabel.isHidden = false
-            regErrorLabel.text = "UserName field is empty"
-            print(regErrorLabel.text!)
-        }
-        
-        /// Password check
-        // MARK: Will add password validation also
-        if passwordTextField.text == "" {
-            passwordTextField.backgroundColor = .red
-            regErrorLabel.isHidden = false
-            regErrorLabel.text = "Missing password"
-            print(regErrorLabel.text!)
-            return
-        } else {
-            // Mark: Add validation here??
-        }
-        
-        /// Is password equals to self  check
-        if checkPasswordTextField.text != passwordTextField.text {
-            regErrorLabel.isHidden = false
-            regErrorLabel.text = "Password missmatched, check again"
+        if password != checkedPassword {
             passwordTextField.backgroundColor = .red
             checkPasswordTextField.backgroundColor = .red
-            checkPasswordTextField.text = ""
-            passwordTextField.text = ""
-            print("Password checking found missmatch")
-            print(regErrorLabel.text!)
+            regErrorLabel.isHidden = false
+            regErrorLabel.text = "Passwords missmatch"
             return
         }
         
-        /// Full Check
-        if regErrorLabel.text == "" {
-            
-            let name = firstNameTextField.text!
-            print("Users firstname now is \(name)")
-            
-            let surname = lastNameTextField.text!
-            print("Users last name now is \(surname)")
-            
-            let userName = usernameTextField.text!
-            print("Users decide \(userName) to be his Username")
-            
-            let password = passwordTextField.text!
-            print("Password added")
-            
-            //            let loginStruct = LoginStruct(firstName: name , lastName: surname , userName: userName , password: password)
-            
-        }
+        if !name.isEmpty && !surName.isEmpty && !password.isEmpty && !checkedPassword.isEmpty && password == checkedPassword {
+        //
+        //        /// Firstname check
+        //        if firstNameTextField.text == "" {
+        //            firstNameTextField.backgroundColor = .red
+        //            regErrorLabel.isHidden = false
+        //            regErrorLabel.text = "First name field is empty"
+        //            print(regErrorLabel.text!)
+        //            return
+        //        }
+        //
+        //        /// Last name check
+        //        if lastNameTextField.text == "" {
+        //            lastNameTextField.backgroundColor = .red
+        //            regErrorLabel.isHidden = false
+        //            regErrorLabel.text = "Last name field is empty"
+        //            print(regErrorLabel.text!)
+        //            return
+        //        }
+        //
+        //        /// UserName check
+        //        if userNameTextField.text == "" {
+        //            userNameTextField.backgroundColor = .red
+        //            regErrorLabel.isHidden = false
+        //            regErrorLabel.text = "User Name field is empty"
+        //            print(regErrorLabel.text!)
+        //        }
+        //
+        //        /// Password check
+        //        // MARK: Will add password validation also
+        //        if passwordTextField.text == "" {
+        //            passwordTextField.backgroundColor = .red
+        //            regErrorLabel.isHidden = false
+        //            regErrorLabel.text = "Missing password"
+        //            print(regErrorLabel.text!)
+        //            return
+        //        } else {
+        //            // Mark: Add validation here??
+        //        }
+        //
+        //        /// Is password equals to self  check
+        //        if checkPasswordTextField.text != passwordTextField.text {
+        //            regErrorLabel.isHidden = false
+        //            regErrorLabel.text = "Password missmatched, check again"
+        //            passwordTextField.backgroundColor = .red
+        //            checkPasswordTextField.backgroundColor = .red
+        //            checkPasswordTextField.text = ""
+        //            passwordTextField.text = ""
+        //            print("Password checking found missmatch")
+        //            print(regErrorLabel.text!)
+        //            return
+        //        }
+        //
+        //        /// Full Check
+        //        if regErrorLabel.text == "" {
+        //
+        //            let name = firstNameTextField.text!
+        //            print("Users firstname now is \(name)")
+        //
+        //            let surName = lastNameTextField.text!
+        //            print("Users last name now is \(surName)")
+        //
+        //            let userName = userNameTextField.text!
+        //            print("Users decide \(userName) to be his Username")
+        //
+        //            let password = passwordTextField.text!
+        //            print("Password added")
+        //
+        //            let loginStruct = LoginStruct(firstName: name , lastName: surName , userName: userName , password: password)
+        //
+        //        }
     }
     
     
@@ -136,7 +179,7 @@ class RegistrationViewController: UIViewController {
 /// Keyboard Delegates
 extension RegistrationViewController: UITextFieldDelegate {
     
-    /// Next field after next button pressed or disapear if no fields left
+    /// Next textField if next button pressed or hides keyboard if no fields left
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         let textFieldTag: Int = textField.tag
         
