eae1dfc39ae2d8f37046c94186c9ed62b6038bc3
Blashko Maksym <blashkoma@gmail.com>
2021-03-11 21:45:02 +0200

    added API update

--- WA_Exam_App/Photos.swift
+++ WA_Exam_App/Photos.swift
@@ -7,13 +7,48 @@
 
 import Foundation
 
+// MARK: - CuratedPhotos
 struct CuratedPhotos: Codable {
-    var page: String
-    var photos: [Photos]
+    let page, perPage: Int
+    let photos: [Photo]
+    let nextPage: String
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case perPage = "per_page"
+        case photos
+        case nextPage = "next_page"
+    }
 }
 
-struct Photos: Codable {
-    var id: Int
-    var url: String
-    var photographer: String
+// MARK: - Photo
+struct Photo: Codable {
+    let id, width, height: Int
+    let url: String
+    let photographer: String
+    let photographerURL: String
+    let photographerID: Int
+    let avgColor: String
+    let src: Src
+    let liked: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id, width, height, url, photographer
+        case photographerURL = "photographer_url"
+        case photographerID = "photographer_id"
+        case avgColor = "avg_color"
+        case src, liked
+    }
+}
+
+// MARK: - Src
+struct Src: Codable {
+    let original, large2X, large, medium: String
+    let small, portrait, landscape, tiny: String
+
+    enum CodingKeys: String, CodingKey {
+        case original
+        case large2X = "large2x"
+        case large, medium, small, portrait, landscape, tiny
+    }
 }
--- WA_Exam_App/Videos.swift
+++ WA_Exam_App/Videos.swift
@@ -8,21 +8,59 @@
 import Foundation
 
 struct PopularVideos: Codable {
-    var page: String
-    var url: String
-    var videos: [Videos]
+    let page, perPage, totalResults: Int
+    let url: String
+    let videos: [Video]
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case perPage = "per_page"
+        case totalResults = "total_results"
+        case url, videos
+    }
 }
 
-struct Videos: Codable {
-    var id: Int
-    var url: String
-    var imageURL: String
-    var duration: Int
-    var videoFiles: [VideoFile]
+// MARK: - Video
+struct Video: Codable {
+    let id, width, height: Int
+    let url: String
+    let image: String
+    let duration: Int
+    let user: User
+    let videoFiles: [VideoFile]
+    let videoPictures: [VideoPicture]
+
+    enum CodingKeys: String, CodingKey {
+        case id, width, height, url, image, duration, user
+        case videoFiles = "video_files"
+        case videoPictures = "video_pictures"
+    }
 }
 
+// MARK: - User
+struct User: Codable {
+    let id: Int
+    let name: String
+    let url: String
+}
+
+// MARK: - VideoFile
 struct VideoFile: Codable {
-    var id: Int
-    var fileType: String
-    var link: String
+    let id: Int
+    let quality, fileType: String
+    let width, height: Int?
+    let link: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, quality
+        case fileType = "file_type"
+        case width, height, link
+    }
+}
+
+// MARK: - VideoPicture
+struct VideoPicture: Codable {
+    let id: Int
+    let picture: String
+    let nr: Int
 }
