6f9ac9a10ca152666af5bf0efdbf9caaf78019f8
Titanscull <ispetrovskyi@gmail.com>
2021-03-12 22:14:31 +0200

    UserEndpointProtocol adeed

---
+++ WA_Exam_App/WA_Exam_App/Model/SignIn LogIn/Network/UserEndPointProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  UserEndPointProtocol.swift
+//  WA_Exam_App
+//
+//  Created by Ievgen Petrovskiy on 12.03.2021.
+//
+
+import Foundation
+
+
+protocol UserEndPointProtocol {
+    func createUser(name: String, surname: String, userName: String, password: String, complition:  @escaping ((User) -> Void))
+    }
