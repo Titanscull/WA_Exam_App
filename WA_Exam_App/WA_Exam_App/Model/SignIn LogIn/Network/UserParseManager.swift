8caa023cec193d1b6bb0f4167838cacc9cacbdd3
Titanscull <ispetrovskyi@gmail.com>
2021-03-13 18:27:26 +0200

    UserParseManager added

---
+++ WA_Exam_App/WA_Exam_App/Model/SignIn LogIn/Network/UserParseManager.swift
@@ -0,0 +1,29 @@
+//
+//  UserParseManager.swift
+//  WA_Exam_App
+//
+//  Created by Ievgen Petrovskiy on 13.03.2021.
+//
+
+import Foundation
+import Parse
+
+class UserParseManager: UserEndPointProtocol {
+    func createUser(name: String, surname: String, userName: String) {
+        <#code#>
+    }
+    
+    func readUser(completion: @escaping (([User]) -> Void)) {
+        <#code#>
+    }
+    
+    func deleteUser(user: User) {
+        <#code#>
+    }
+    
+    func updateUser(user: User) {
+        <#code#>
+    }
+    
+    
+}
