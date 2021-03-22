08d7923a308c7b6c47f6cd20cc7487ef32ec6367
Blashko Maksym <blashkoma@gmail.com>
2021-03-22 11:41:29 +0200

    added Detail VC

---
+++ WA_Exam_App/WA_Exam_App/Controller/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  WA_Exam_App
+//
+//  Created by Blashko Maksim on 22.03.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
