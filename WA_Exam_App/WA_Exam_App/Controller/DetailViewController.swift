d955f71cdb339aff0dea8e96a46c4ba12911054c
Blashko Maksym <blashkoma@gmail.com>
2021-03-27 02:26:45 +0200

    added test for web view

--- WA_Exam_App/WA_Exam_App/Controller/DetailViewController.swift
+++ WA_Exam_App/WA_Exam_App/Controller/DetailViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import WebKit
+import AVKit
 
 class DetailViewController: UIViewController {
 
@@ -15,6 +16,22 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // test 1
         detailView.loadFileURL(URL(fileURLWithPath: "https://www.pexels.com/video/2499611/"), allowingReadAccessTo: URL(fileURLWithPath: "https://www.pexels.com/video/2499611/"))
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+
+        // test 2
+        super.viewDidAppear(animated)
+
+        let avVC = AVPlayerViewController()
+        let item = AVPlayerItem(asset: AVAsset(url: URL(fileURLWithPath: "https://www.pexels.com/video/2499611/")))
+        avVC.player = AVPlayer(playerItem: item)
+        avVC.player?.play()
+        
+        present(avVC, animated: true, completion: nil)
+        
+   }
 }
